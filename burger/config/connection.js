934c7d5e434f7627b3e037018fd0561569b823f8
Nick Donato <42154985+njd1228@users.noreply.github.com>
2018-11-28 17:28:29 -0500

    Add files via upload

---
+++ burger/config/connection.js
@@ -0,0 +1,20 @@
+// Connecting Node to burgers_db in MySQL 
+// and then exporting the connection
+var mysql = require('mysql');
+var connection;
+
+// Setting up connection for use on Heroku
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+// Localhost connection
+else {
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'burgers_db'
+  });
+};
+
+module.exports = connection; 
