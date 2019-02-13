7997a074982f1ab97fbe5361c6334c791af1a2fa
Nick <njd5059@gmail.com>
2019-02-13 14:26:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,24 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override')
+var express = require("express");
 
+var PORT = process.env.PORT || 8000;
 var app = express();
-//Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + '/public'));
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-	extended: false
-}))
-// override with POST having ?_method=DELETE
-app.use(methodOverride('_method'))
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-var routes = require('./controllers/burgers_controller.js');
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use('/', routes);
-app.use('/update', routes);
-app.use('/create', routes);
+var exphbs = require("express-handlebars");
 
-var PORT = process.env.PORT || 3036;
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
 app.listen(PORT, function() {
-
-console.log(module.exports)
+  console.log("Listening on port:%s", PORT);
+});
